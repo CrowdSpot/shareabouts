b14bc9c5fda7790cfb60590bcc3494fbfa1e73f8
mark hellewell <mark.hellewell@icloud.com>
2013-05-21 17:42:59 +1000

    added web-2 host

--- roles.py
+++ roles.py
@@ -1,6 +1,6 @@
 vhosts = {
     'theage.crowdspot.com.au': {
-        'hosts': ['web-1.crowdspot.com.au'],
+        'hosts': ['web-1.crowdspot.com.au', 'web-2.crowdspot.com.au'],
         'manage': './src/manage.py',
         'settings': 'src.project.settings',
         'requirements': 'src/requirements.txt',
