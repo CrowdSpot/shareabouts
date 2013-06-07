f50abb0b584d6789a2e83e3e05699dbf36d7d92e
mark hellewell <mark.hellewell@icloud.com>
2013-06-07 16:49:08 +1000

    added beactive to roles.py

--- roles.py
+++ roles.py
@@ -3,6 +3,10 @@ vhosts = {
         'hosts': ['web-1.crowdspot.com.au', 'web-3.crowdspot.com.au'],
         'manage': './src/manage.py',
         'settings': 'src.project.settings',
-        'requirements': 'src/requirements.txt',
+    },
+    'beactive.crowdspot.com.au': {
+        'hosts': ['web-1.crowdspot.com.au', 'web-3.crowdspot.com.au'],
+        'manage': './src/manage.py',
+        'settings': 'src.project.settings',
     },
 }
