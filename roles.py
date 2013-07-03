2775c2a85f2cee6b7ab786256820a18bc0e1f3e6
mark hellewell <mark.hellewell@icloud.com>
2013-07-03 11:36:48 +1000

    beactive -> trailblazer in roles.py

--- roles.py
+++ roles.py
@@ -4,7 +4,7 @@ vhosts = {
         'manage': './src/manage.py',
         'settings': 'src.project.settings',
     },
-    'beactive.crowdspot.com.au': {
+    'trailblazer.crowdspot.com.au': {
         'hosts': ['web-1.crowdspot.com.au', 'web-3.crowdspot.com.au'],
         'manage': './src/manage.py',
         'settings': 'src.project.settings',
