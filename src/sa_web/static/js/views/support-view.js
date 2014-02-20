0dbe158bb371056e1db7fafe76f0f3d20dc451c0
dan peade <dan.peade@gmail.com>
2014-02-20 21:32:50 +1100

    Hacks to force users to provide an email address in order to support a spot, as well as to make the header image link to the PTNT website.

--- src/sa_web/static/js/views/support-view.js
+++ src/sa_web/static/js/views/support-view.js
@@ -60,13 +60,24 @@ var Shareabouts = Shareabouts || {};
       if (checked) {
         $form = this.$('form'),
         attrs = S.Util.getAttrs($form);
-        this.collection.create(attrs, {
-          wait: true,
-          error: function() {
-            self.getSupportStatus(self.options.userToken).destroy();
-            alert('Oh dear. It looks like that didn\'t save.');
-          }
-        });
+
+        //PLACES-SUPPORT-HACK
+        attrs.email_address = $('#user_support #email_address').val();
+        var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+        if (pattern.test(attrs.email_address)) {
+            this.collection.create(attrs, {
+              wait: true,
+              error: function() {
+                self.getSupportStatus(self.options.userToken).destroy();
+                alert('Oh dear. It looks like that didn\'t save.');
+              }
+            });
+        } else {
+            alert("Please enter a valid email address to support this spot. Your email will be kept confidential and will only be used to communicate results of this project and campaign for better public transport.")
+            evt.target.checked = false;
+            this.render()
+        }
+
       } else {
         userSupport = this.userSupport;
         this.userSupport.destroy({
