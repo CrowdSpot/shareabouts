6c6c91606dfdd1beca01b6b213eebeceb428da69
Anthony <anthony@crowdspot.com.au>
2015-12-08 12:16:16 +1100

    Updated gatekeeper.js file

--- src/sa_web/static/libs/gatekeeper.js
+++ src/sa_web/static/libs/gatekeeper.js
@@ -1,3 +1,4 @@
+// 0.1.2
 /*globals $ */
 
 var Gatekeeper = {};
@@ -5,24 +6,58 @@ var Gatekeeper = {};
 (function(NS) {
 
   NS.getInvalidFormEls = function(form) {
-    var invalidEls, val;
+     var $form = $(form),
+         invalidEls;
 
     invalidEls = $(form).find('input, select, textarea').map(function() {
+      var $this = $(this),
+          $checkableGroup,
+          $requiredInputs,
+          isCheckbox,
+          restoreRequired,
+          hasValue;
+          
       // Only validate visible elements
       if ($(this).is(':visible')) {
 
+        // For checkbox groups, only one needs to be checked
+        isCheckbox = $this.is('[type="checkbox"]');
+        if (isCheckbox) {
+          $requiredInputs = $form.find('[name="'+$this.attr('name')+'"][required]');
+          hasValue = $requiredInputs.is(':checked');
+          if (hasValue) {
+            $requiredInputs.removeAttr('required');
+          }
+        }
+
+        restoreRequired = function() {
+          if ($requiredInputs) {
+            $requiredInputs.attr('required', 'required');
+          }
+        };
+       
         // Does it support the validity object?
         if (this.validity) {
           // Add it to the array if it's invalide
           if (!this.validity.valid) {
+            restoreRequired();
             return this;
           }
         } else {
-          // Strip whitespace from the value
-          val = (this.value || '').replace(/\s+/);
+          $this.removeClass('gatekeeper-invalid');
+ 
+          if ($this.is('[type="checkbox"]') || $this.is('[type="radio"]')) {
+            $checkableGroup = $form.find('[name="'+$this.attr('name')+'"]');
+            hasValue = $checkableGroup.is(':checked');
+          } else {
+            // Strip whitespace from the value
+            hasValue = (this.value || '').replace(/\s+/) !== '';
+          }
 
           // Manually support 'required' for old browsers
-          if (this.hasAttribute('required') && val === '') {
+          if (this.hasAttribute('required') && !hasValue) {
+            $this.addClass('gatekeeper-invalid');
+            restoreRequired();
             return this;
           }
         }
