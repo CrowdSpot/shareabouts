18a3a0c7e565e15c03d1e0425c941f51bdf325c8
Anthony <anthony@crowdspot.com.au>
2016-06-16 16:45:52 +1000

    Revert "Geocoding update"
    This reverts commit 2b40c4e77d040c5e2ffe2e06b099b9fd85d8d3d3.

--- src/sa_web/static/js/views/app-view.js
+++ src/sa_web/static/js/views/app-view.js
@@ -198,12 +198,7 @@ var Shareabouts = Shareabouts || {};
       // is enabled.
       $(S).on('reversegeocode', function(evt, locationData) {
         var locationString = Handlebars.templates['location-string'](locationData);
-<<<<<<< HEAD
         self.geocodeAddressView.setAddress($.trim(locationString));
-=======
-        self.geocodeAddressView.setAddress(locationString.trim());
-        self.placeFormView.setLatLng(locationData.latLng);
->>>>>>> parent of 9144212... Geocoding update
         self.placeFormView.setLocation(locationData);
       });
 
